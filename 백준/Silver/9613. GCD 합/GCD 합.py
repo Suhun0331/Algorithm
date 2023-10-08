2854d1b040766f3a60ee98211fcd7d52db8c37c1
Suhun0331 <104614675+Suhun0331@users.noreply.github.com>
2023-10-08 18:49:06 +0900

    [Silver IV] Title: GCD 합, Time: 40 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Silver/9613. GCD 합/GCD 합.py
+++ 백준/Silver/9613. GCD 합/GCD 합.py
@@ -10,3 +10,21 @@ for i in range(num):
         for k in range(j+1, len(lst)):
             summ += (math.gcd(lst[j],lst[k]))
     print(summ)
+
+
+''' 유클리드 호제법 풀
+def gcd(a, b):
+    while a%b != 0:
+        a, b = b, a%b
+    return b
+
+num = int(input())
+
+for i in range(num):
+    lst = list(map(int,input().split()))
+    summ = 0
+    for j in range(1, len(lst)-1):
+        for k in range(j+1, len(lst)):
+            summ += (gcd(lst[j],lst[k]))
+    print(summ)
+'''
