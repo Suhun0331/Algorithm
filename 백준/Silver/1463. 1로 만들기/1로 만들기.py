928c579f8db7a4fb5d1f202695bce85336872653
Suhun0331 <104614675+Suhun0331@users.noreply.github.com>
2023-10-10 14:20:07 +0900

    [Silver III] Title: 1로 만들기, Time: 488 ms, Memory: 39068 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -6,12 +6,15 @@ lst = [0]*(n+1)
 
 
 for i in range(2, n+1):
-    lst[i] = lst[i-1]+1
 
-    if i%2 == 0:
-        lst[i] = min(lst[i//2]+1, lst[i])
-    if i%3 == 0:
-        lst[i] = min(lst[i//3]+1, lst[i])
+    if i%2 == 0 and i%3 == 0:
+        lst[i] = min(lst[i//2],lst[i//3], lst[i-1])+1
+    elif i%3 == 0:
+        lst[i] = min(lst[i//3], lst[i-1])+1
+    elif i%2 == 0:
+        lst[i] = min(lst[i//2], lst[i-1])+1
+    else:
+        lst[i] = lst[i-1]+1
     
 
 print(lst[n])
