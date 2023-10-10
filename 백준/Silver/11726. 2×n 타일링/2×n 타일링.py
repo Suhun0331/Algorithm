02b10c02c7141ce7dcf7aece2861fde4de18e38e
Suhun0331 <104614675+Suhun0331@users.noreply.github.com>
2023-10-10 16:35:54 +0900

    [Silver III] Title: 2×n 타일링, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -1,17 +1,13 @@
 import sys
-
 n = int(sys.stdin.readline())
 
 lst = [0]*1001
 
-def df(n):
-    if n == 1:
-        return 1
-    if n == 2:
-        return 2
-    if lst[n] != 0:
-        return lst[n]
-    else:
-        lst[n] = (df(n-1) + df(n-2))%10007
-        return lst[n]
-print(df(n))
+for i in range(1,1001):
+    if i == 1:
+        lst[i] = 1
+    if i == 2:
+        lst[i] = 2
+    if i>=3:
+        lst[i] = (lst[i-1] + lst[i-2])%10007
+print(lst[n])
