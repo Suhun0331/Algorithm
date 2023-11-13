871e06242f308103b8ed7f83359adc5c5087f0a2
Suhun0331 <104614675+Suhun0331@users.noreply.github.com>
2023-11-13 14:19:29 +0900

    [Silver I] Title: 카드 구매하기, Time: 80 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -1,18 +1,15 @@
-import sys
-n = int(sys.stdin.readline())
+n = int(input())
+lst = [0]*(n+1)
+maxx = [0] * (n+1)
 
-cost = list(map(int, sys.stdin.readline().split()))
-maxx = [0]*(n+1)
-cost.insert(0,0)
-
+lst = list(map(int, input().split()))
+lst.insert(0,0)
 for i in range(1, n+1):
     if i == 1:
-        maxx[i] = cost[i]
+        maxx[i] = lst[i]
     else:
-        maxx[i] = cost[i]
+        maxx[i] = lst[i]
         for j in range(1, i//2+1):
-            if maxx[i]<maxx[j]+maxx[i-j]:
+            if maxx[i] < maxx[j]+maxx[i-j]:
                 maxx[i] = maxx[j]+maxx[i-j]
-                
-
 print(maxx[n])
