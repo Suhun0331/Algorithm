0c2455736f1d38a245212774c51092bc3b07598c
Suhun0331 <104614675+Suhun0331@users.noreply.github.com>
2024-03-04 12:16:25 +0900

    [Silver V] Title: 단어 정렬, Time: 824 ms, Memory: 33324 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,12 +1,13 @@
-num = int(input())
+a = int(input())
 
 lst = []
-for i in range(num):
-    a = input()
-    if a not in lst:
-        lst.append(a)
-lst.sort()
-lst.sort(key = len)
 
-for i in lst:
-    print(i)
+for i in range(a):
+    lst.append(input())
+lst = sorted(lst)
+lst = sorted(lst, key = len)
+
+print(lst[0])
+for i in range(1, len(lst)):
+    if lst[i] != lst[i-1]:
+        print(lst[i])
