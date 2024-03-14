abe2dc71efcdea7c2bce2fe25fdada9ae1d9ce62
Suhun0331 <104614675+Suhun0331@users.noreply.github.com>
2024-03-14 11:38:13 +0900

    [Silver IV] Title: 스택, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,37 +1,30 @@
 import sys
 
-num = int(input())
+n = int(sys.stdin.readline())
 
-lst = []
+stack = []
 
-for _ in range(num):
-    a = list(map(str, sys.stdin.readline().split()))
-    if len(a) == 1:
-        ans = a[0]
-    elif len(a) == 2:
-        ans = a[0]
-        ans2 = a[1]
-        
-    if ans == 'push':
-        lst.append(int(ans2))
-        
-    if ans == 'pop' and len(lst) != 0:
-        number = lst[len(lst)-1]
-        del lst[len(lst)-1]
-        print(number)
-    elif ans == 'pop':
-        print(-1)
-        
-        
-    if ans == 'top' and len(lst) != 0:
-        print(lst[len(lst)-1])
-    elif ans == 'top':
-        print(-1)
+for i in range(n):
+    ans = list(sys.stdin.readline().split())
+    if ans[0] == 'push':
+        num = int(ans[1])
+        stack.append(num)
 
-    if ans == 'empty'and len(lst) == 0:
-        print(1)
-    elif ans == 'empty'and len(lst) != 0:
-        print(0)
-
-    if ans == 'size':
-        print(len(lst))
+    elif ans[0] == 'pop':
+        if stack:
+            print(stack[-1])
+            del stack[-1]
+        else:
+            print(-1)
+    elif ans[0] == 'size':
+        print(len(stack))
+    elif ans[0] == 'empty':
+        if stack:
+            print(0)
+        else:
+            print(1)
+    elif ans[0] == 'top':
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
