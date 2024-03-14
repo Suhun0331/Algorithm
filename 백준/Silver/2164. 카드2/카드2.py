5c24c5e92e8c8594cd0bd855f65df9e7961e079b
Suhun0331 <104614675+Suhun0331@users.noreply.github.com>
2024-03-14 14:12:53 +0900

    [Silver IV] Title: 카드2, Time: 204 ms, Memory: 50928 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -3,7 +3,10 @@ from collections import deque
 
 n = int(sys.stdin.readline())
 
-lst = deque([i for i in range(1, n+1)])
+lst = deque()
+for i in range(1, n+1):
+    lst.append(i)
+
 
 
 while(len(lst) != 1):
