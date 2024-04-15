c2fb1ee6b826055f662efcff16fe139a18c6ea29
Suhun0331 <104614675+Suhun0331@users.noreply.github.com>
2024-04-15 13:59:42 +0900

    [Silver I] Title: 절댓값 힙, Time: 124 ms, Memory: 35724 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -4,14 +4,25 @@ import heapq
 input = sys.stdin.readline
 
 num = int(input())
-heap = []
+max_heap = []
+min_heap = []
 
 for _ in range(num):
     n = int(input())
     if n == 0:
-        if heap:
-            print(heapq.heappop(heap)[1])
+        if max_heap and min_heap:
+            if abs(max_heap[0]) < abs(min_heap[0]):
+                print(heapq.heappop(max_heap))
+            else:
+                print(heapq.heappop(min_heap)*(-1))
+        elif max_heap:
+            print(heapq.heappop(max_heap))
+        elif min_heap:
+            print(heapq.heappop(min_heap)*(-1))
         else:
             print(0)
     else:
-        heapq.heappush(heap, (abs(n),n))
+        if n > 0:
+            heapq.heappush(max_heap,n)
+        else:
+            heapq.heappush(min_heap,-n)
