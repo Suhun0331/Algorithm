41a7d0c2284c17e18a65127bf3a2a454a6f92bbd
Suhun0331 <104614675+Suhun0331@users.noreply.github.com>
2024-04-15 18:24:21 +0900

    [Gold II] Title: 가운데를 말해요, Time: 224 ms, Memory: 37132 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.py
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.py
@@ -7,27 +7,19 @@ right_heap = []
 cnt = 1
 num = int(input())
 
-for _ in range(num):
+for i in range(1, num+1):
     n = int(input())
-    if cnt%2 == 1:
-        cnt += 1
-        heapq.heappush(left_heap, -n)
-        if cnt > 2:
-            if -left_heap[0] > right_heap[0]:
-                left = heapq.heappop(left_heap)
-                right = heapq.heappop(right_heap)
-                heapq.heappush(left_heap, -right)
-                heapq.heappush(right_heap, -left)
-                
 
-        print(-left_heap[0])    
+    if i%2 == 1:
+        heapq.heappush(left_heap, -n)
     else:
-        cnt += 1
         heapq.heappush(right_heap, n)
+        
+    if i > 1:
         if -left_heap[0] > right_heap[0]:
             left = heapq.heappop(left_heap)
             right = heapq.heappop(right_heap)
             heapq.heappush(left_heap, -right)
             heapq.heappush(right_heap, -left)
 
-        print(-left_heap[0])
+    print(-left_heap[0])
