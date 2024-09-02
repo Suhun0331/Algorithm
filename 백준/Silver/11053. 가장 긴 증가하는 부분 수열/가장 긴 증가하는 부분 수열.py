100bbb5f1185053b1678a7350cd157d692ca40fe
SuHun <104614675+Suhun0331@users.noreply.github.com>
2024-09-02 22:17:55 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 96 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -10,17 +10,11 @@ arr.insert(0,0)
 dp = [0] * (n+1)
 dp[1] = 1
 
-'''
-for i in range(2, n+1):
+for i in range(1, n+1):
     for j in range(1, i):
-        if arr[i] > arr[j] and dp[i] < dp[j]:
-            dp[i] = dp[j]
-    dp[i] += 1
-'''
-
-
-for i in range(2, n+1):
-    for j in range(1, i):
+        if i == 1:
+            dp[i] = 1
+            
         if arr[i] > arr[j] and dp[i] <= dp[j]:
             dp[i] = dp[j]+1
     if dp[i] == 0:
