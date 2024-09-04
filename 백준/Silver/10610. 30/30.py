15e368e3d7d29009eaae89c8043545687270be29
SuHun <104614675+Suhun0331@users.noreply.github.com>
2024-09-04 14:04:38 +0900

    [Silver IV] Title: 30, Time: 400 ms, Memory: 35780 KB -BaekjoonHub

---
+++ 백준/Silver/10610. 30/30.py
@@ -0,0 +1,19 @@
+import heapq
+
+n = list(map(int, input()))
+heap = []
+
+for i in n:
+    heapq.heappush(heap, -i)
+
+#print(n)
+#print(sum(n))
+num = ''
+if sum(n)%3 != 0 or 0 not in n:
+    print(-1)
+else:
+    while heap:
+        num += str((-heapq.heappop(heap)))
+
+    print(int(num))
+
