8e0dedb774da5d07220789add74927cbf58339af
SuHun <104614675+Suhun0331@users.noreply.github.com>
2024-09-05 17:39:46 +0900

    [Silver I] Title: 데스 나이트, Time: 72 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Silver/16948. 데스 나이트/데스 나이트.py
+++ 백준/Silver/16948. 데스 나이트/데스 나이트.py
@@ -1,3 +1,9 @@
+'''
+처음에 엄청난 하드코딩 조지다가 이건 아니다 싶어서
+moves로 이동 배열 만듦
+중복 코드가 좀 많은 거 같다
+최적화 하는 방법 찾아봐야지
+'''
 from collections import deque
 
 n = int(input())
@@ -15,29 +21,28 @@ move_count = [[0 for i in range(n)] for i in range(n)]
 visit[a][b] = True
 queue.append((a,b))
 finish = False
+
 while queue:
     r, c = queue.popleft()
     #print(r, c)
     for move in moves:
         x = r + move[0]
         y = c + move[1]
-        if x == end[0] and y == end[1]:
-            finish = True
-            move_count[x][y] = move_count[r][c] + 1
-            break
-        if x<0 or x>n-1 or y<0 or y>n-1:
+        if x<0 or x>n-1 or y<0 or y>n-1 or visit[x][y] == True:
             continue
-        if visit[x][y] == True:
-            continue
+        
         visit[x][y] = True
         queue.append((x, y))
         move_count[x][y] = move_count[r][c] + 1
         
     #print(queue)
 
-    if finish == True:
-        print(move_count[end[0]][end[1]])
+    if move_count[end[0]][end[1]] != 0:
         break
-if finish == False:    
+
+
+if move_count[end[0]][end[1]] != 0:
+    print(move_count[end[0]][end[1]])
+else:
     print(-1)
 
