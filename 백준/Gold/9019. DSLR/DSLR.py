45999ae37bafa913d93f882f10d7d8f0929934a1
SuHun <104614675+Suhun0331@users.noreply.github.com>
2024-09-08 18:19:37 +0900

    [Gold IV] Title: DSLR, Time: 4252 ms, Memory: 214064 KB -BaekjoonHub

---
+++ 백준/Gold/9019. DSLR/DSLR.py
@@ -0,0 +1,53 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+command = ['D', 'S', 'L', 'R']
+
+for i in range(n):
+    queue = deque()
+    record = ['']*10001
+    visit = [False] * 10001
+ 
+    start, end = map(int, input().split())
+    queue.append(start)
+    visit[start] = True
+
+    while queue:
+        substart = queue.popleft()
+        
+        if substart == end:
+            print(record[end])
+            break
+
+        sub = (substart*2)%10000
+        if not visit[sub]:
+            visit[sub] = True
+            queue.append(sub)
+            record[sub] = record[substart] + 'D'
+
+        if substart == 0:
+            sub = 9999
+        else:
+            sub = substart - 1
+        if not visit[sub]:
+            visit[sub] = True
+            queue.append(sub)
+            record[sub] = record[substart] + 'S'
+
+
+        sub = substart // 1000 + (substart % 1000)*10
+        if not visit[sub]:
+            visit[sub] = True
+            queue.append(sub)
+            record[sub] = record[substart] + 'L'
+
+        sub = substart // 10 + (substart % 10) * 1000
+        if not visit[sub]:
+            visit[sub] = True
+            queue.append(sub)
+            record[sub] = record[substart] + 'R'
+
+
