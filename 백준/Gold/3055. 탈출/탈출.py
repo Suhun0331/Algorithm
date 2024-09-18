881a87b161357836c7fee537f363ec72055ed91b
SuHun <104614675+Suhun0331@users.noreply.github.com>
2024-09-18 18:36:09 +0900

    [Gold IV] Title: 탈출, Time: 60 ms, Memory: 34136 KB -BaekjoonHub

---
+++ 백준/Gold/3055. 탈출/탈출.py
@@ -0,0 +1,72 @@
+'''
+
+'''
+
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+r, c = map(int, input().split())
+
+map = [list(map(str, input().strip())) for i in range(r)]
+
+start = []
+end = []
+water = []
+q = deque()
+for i in range(r):
+    for j in range(c):
+        if map[i][j] == 'D':
+            end.append([i, j])
+        if map[i][j] == 'S':
+            start.append([i,j])
+            q.append([i, j])
+
+
+for i in range(r):
+    for j in range(c):    
+        if map[i][j] == '*':
+            water.append([i,j])
+            q.append([i, j])
+
+
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+visited = [[-1]*c for i in range(r)]
+visited[start[0][0]][start[0][1]] = 0
+#print(visited)
+
+while q:
+    x, y = q.popleft()
+    
+    for i in range(4):
+        xx = x + dx[i]
+        yy = y + dy[i]
+        if xx < 0 or xx >= r or yy < 0 or yy >= c:
+            continue
+        if map[x][y] == 'S' and (map[xx][yy] == '.' or map[xx][yy] == 'D'):
+            if map[xx][yy] == 'D':
+                print(visited[x][y] + 1)
+                sys.exit()
+            map[xx][yy] = 'S'
+            visited[xx][yy] = visited[x][y] + 1
+            q.append([xx, yy])
+            #print(q)
+            
+        elif map[x][y] == '*' and (map[xx][yy] == '.' or map[xx][yy] == 'S'):
+            map[xx][yy] = '*'
+            q.append([xx, yy])
+
+    
+    #print(map)
+
+
+if visited[end[0][0]][end[0][1]] == -1:
+    print('KAKTUS')
+else:
+    print(visited[end[0][0]][end[0][1]])
+
+
+
+            
+    
