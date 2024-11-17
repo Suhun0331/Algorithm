6aae9a9cb400a09c8ab29e10aaa075994755a9f8
SuHun <104614675+Suhun0331@users.noreply.github.com>
2024-11-17 14:49:58 +0900

    [Silver I] Title: 미로 탐색, Time: 104 ms, Memory: 110744 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -5,25 +5,21 @@ for i in range(n):
     arr.append(list(map(int, input())))
 
 visited = [[0]*m for _ in range(n)]
-di = [1, 0, -1, 0]
-dj = [0, 1, 0, -1]
-def bfs(si, sj, ei, ej):
 
+
+def bfs(si, sj, ei, ej):
     q = []
     q.append((si, sj))
     visited[si][sj] = 1
     while q:
         ci, cj = q.pop(0)
-        if ci == ei and cj == ej:
-            return visited[ci][cj]
-
-        for k in range(4):
-            ni = ci + di[k]
-            nj = cj + dj[k]
-            if 0<= ni< n and 0<= nj < m and not visited[ni][nj] and arr[ni][nj] == 1:
+        if (ci, cj) == (ei , ej):
+            return arr[ei][ej]
+        for di, dj in ((-1, 0), (1, 0), (0, 1), (0, -1)):
+            ni = ci + di
+            nj = cj + dj
+            if 0 <= ni < n and 0 <= nj < m and visited[ni][nj] == 0 and arr[ni][nj] == 1:
                 q.append((ni, nj))
-                visited[ni][nj] = visited[ci][cj] + 1
+                arr[ni][nj] = arr[ci][cj] + 1
 
-
-ans = bfs(0, 0, n-1, m-1)
-print(ans)
+print(bfs(0, 0, n-1, m-1))
