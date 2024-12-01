162b4bcd459505641882f325f49b370717ed3366
SuHun <104614675+Suhun0331@users.noreply.github.com>
2024-12-01 21:55:38 +0900

    [Silver II] Title: 이동하기, Time: 812 ms, Memory: 78676 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.py
+++ 백준/Silver/11048. 이동하기/이동하기.py
@@ -3,19 +3,11 @@ arr = []
 for _ in range(n):
     arr.append(list(map(int, input().split())))
     
-dp = [[0]*m for _ in range(n)]
-dp[0][0] = arr[0][0]
+dp = [[0]*(m+1) for _ in range(n+1)]
 
-for i in range(n):
-    for j in range(m):
-        if (i, j) == (n-1, m-1):
-            break
-        
-        if i+1 < n:
-            dp[i+1][j] = max(dp[i][j] + arr[i+1][j], dp[i+1][j])
-        if j+1 < m:
-            dp[i][j+1] = max(dp[i][j] + arr[i][j+1], dp[i][j+1])
-        if i+1 < n and j+1 < m:
-            dp[i+1][j+1] = max(dp[i][j] + arr[i+1][j+1], dp[i+1][j+1])
 
-print(dp[n-1][m-1])
+for i in range(1, n+1):
+    for j in range(1, m+1):
+        dp[i][j] = max(dp[i-1][j], dp[i-1][j-1], dp[i][j-1]) +arr[i-1][j-1] 
+
+print(dp[n][m])
