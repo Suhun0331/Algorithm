b692f4a83e55a4f366d2e5a99c3fe8ae08144056
SuHun <104614675+Suhun0331@users.noreply.github.com>
2024-12-10 13:41:33 +0900

    [Gold V] Title: 평범한 배낭, Time: 4216 ms, Memory: 230376 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -6,7 +6,7 @@ for i in range(1, n+1):
     w, v = map(int, input().split())
     for j in range(k+1):
         if j-w >= 0:
-            arr[i][j] = max(arr[i][j-1], arr[i-1][j], arr[i-1][j-w] + v)
+            arr[i][j] = max(arr[i-1][j], arr[i-1][j-w] + v)
         else:
-            arr[i][j] = max(arr[i][j-1], arr[i-1][j])
+            arr[i][j] = arr[i-1][j]
 print(arr[n][k])
