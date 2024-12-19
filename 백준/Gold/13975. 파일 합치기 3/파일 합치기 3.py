ff23adf7418338877a402cc2828e8dc7d43de334
SuHun <104614675+Suhun0331@users.noreply.github.com>
2024-12-19 13:51:15 +0900

    [Gold IV] Title: 파일 합치기 3, Time: 4840 ms, Memory: 138080 KB -BaekjoonHub

---
+++ 백준/Gold/13975. 파일 합치기 3/파일 합치기 3.py
@@ -0,0 +1,20 @@
+#from collections import deque
+import heapq
+t = int(input())
+for _ in range(t):
+
+    k = int(input())
+
+    arr = list(map(int, input().split()))
+
+    heapq.heapify(arr)
+
+    #arr = deque(sorted(arr))
+    answer = 0
+    while len(arr) != 1:
+        #print(arr, answer)
+        a = heapq.heappop(arr) + heapq.heappop(arr)
+        answer += a
+        heapq.heappush(arr, a)
+        
+    print(answer)
