cdf03ab250831632e0fb56eaf26796762b3899bd
SuHun <104614675+Suhun0331@users.noreply.github.com>
2024-12-23 22:22:47 +0900

    [Silver II] Title: 유기농 배추, Time: 212 ms, Memory: 32776 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,33 +1,34 @@
 import sys
 sys.setrecursionlimit(10 ** 6)
-
-case = int(input())
-
+t = int(input())
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+#(n, m) -> (y, x)
+count = 0
 def dfs(x, y):
-    if x<0 or x>=col or y<0 or y>=row:
-        return False
-    if graph[y][x] == 1:
-        graph[y][x] = 0
-        dfs(x-1, y)
-        dfs(x+1, y)
-        dfs(x, y-1)
-        dfs(x, y+1)
-        return True
-    return False
+    global count
+    visited[x][y] = True
+    for i in range(4):
+        xx = x + dx[i]
+        yy = y + dy[i]
+        if 0<= xx < n and 0 <= yy < m and not visited[xx][yy] and arr[xx][yy] == 1:
+            dfs(xx, yy)
 
-for _ in range(case):
-    col, row, baechu = map(int, input().split())
-    graph = []
-    for i in range(row):
-        graph.append([0]*col)
-    for i in range(baechu):
-        a, b = map(int, input().split())
-        graph[b][a] = 1
+for _ in range(t):
+    count = 0
+    m, n, k = map(int, input().split())
 
-        
-    result = 0
-    for i in range(row):
-        for j in range(col):
-            if dfs(j, i) == True:
-                result += 1
-    print(result)
+    arr = [[0]*m for _ in range(n)]
+    visited = [[False]*m for _ in range(n)]
+
+    for _ in range(k):
+        x, y = map(int, input().split())
+        arr[y][x] = 1
+
+    for i in range(n):
+        for j in range(m):
+            if visited[i][j] == False and arr[i][j] == 1:
+                dfs(i, j)
+                count += 1
+    print(count)
+
