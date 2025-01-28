437f4e24c6575e6b779159de81f6d1d60ef7f06b
SuHun <104614675+Suhun0331@users.noreply.github.com>
2025-01-28 18:19:21 +0900

    [Gold IV] Title: N-Queen, Time: 28252 ms, Memory: 198364 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,23 +1,28 @@
+import sys
+
+input = sys.stdin.readline
+
 n = int(input())
 
-lst = [0]*n
 ans = 0
-def promising(x):
+lst = [0] * (n+1)
+
+def check(x):
     for i in range(x):
-        if lst[i]==lst[x] or abs(lst[i]-lst[x])==abs(i-x):
+        if lst[x] == lst[i] or abs(lst[x] - lst[i]) == abs(x-i):
             return False
     return True
 
 def dfs(x):
     global ans
-    if n == x:
+    if x == n:
         ans += 1
         return
     else:
         for i in range(n):
             lst[x] = i
-            if promising(x):
+            if check(x):
                 dfs(x+1)
+
 dfs(0)
 print(ans)
-        
