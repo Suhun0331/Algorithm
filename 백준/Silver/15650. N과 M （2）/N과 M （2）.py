1f763c26a92bb1f3b3d956990176a3bb1e5450ba
SuHun <104614675+Suhun0331@users.noreply.github.com>
2025-02-03 11:59:51 +0900

    [Silver III] Title: N과 M (2), Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -5,25 +5,16 @@ s = []
 check = []
 def dfs(start):
     if len(s) == m:
-        '''
-        sort_s = sorted(s)
-        for j in check:
-            if sort_s == j:
-                return
-        check.append(s[:])
-        '''
         
         for i in s:
             print(i, end = ' ')
         print()
         return
     for i in range(start, n+1):
-        if visited[i]:
+        if i in s:
             continue
         s.append(i)
-        visited[i] = True
         dfs(i)
         s.pop()
-        visited[i] = False
 
 dfs(1)
