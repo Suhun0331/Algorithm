804e56466432a227fd031936d01f226398a23ce6
SuHun <104614675+Suhun0331@users.noreply.github.com>
2025-02-04 16:16:05 +0900

    [level 1] Title: 폰켓몬, Time: 0.75 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,17 +1,5 @@
 def solution(nums):
-    answer = 0
     
-    arr = []
-    count = 0
-    
-    for i in nums:
-        if i not in arr:
-            arr.append(i)
-    
-    if len(nums)//2 <= len(arr):
-        answer = len(nums)//2
-    else:
-        answer = len(arr)
 
             
-    return answer
+    return min(len(nums)//2 , len(set(nums)))
