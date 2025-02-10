8030c7b29490cebf88670ee97613447c10e6cd36
SuHun <104614675+Suhun0331@users.noreply.github.com>
2025-02-10 11:21:03 +0900

    [level 2] Title: H-Index, Time: 0.23 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,19 +1,13 @@
 '''
-오름차순 정렬 -> 0 1 3 5 6
-배열 내 현 위치 index 변수 생성하고, i가 배열[index]보다 크거나 같아지면 index + 1
-for i in range(0, arr[마지막]) -> 전체 길이 - index가 i보다 크면 answer = i
+검색한 풀이
 '''
 
-def solution(citations): #10 11 13 13 15 16
-    answer = 0
-    citations.sort()
-    index = 0
-    for i in range(citations[-1]): # 0 ~ 15
+def solution(citations):
+    
+    citations.sort(reverse = True) # 6 5 2 1 0 -> 2
+    
+    for i in range(len(citations)):
+        if i+1 > citations[i]:
+            return i
         
-        if len(citations) - index >= i and index <= i:
-            answer = i
-        
-        if citations[index] <= i:
-            index += 1
-        
-    return answer
+    return len(citations)
