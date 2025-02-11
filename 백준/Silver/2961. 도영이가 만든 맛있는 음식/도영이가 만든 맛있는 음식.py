0015281b9b58c39836032aca2859d702ae4266c1
SuHun <104614675+Suhun0331@users.noreply.github.com>
2025-02-11 16:25:05 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.py
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.py
@@ -10,14 +10,6 @@ for i in range(n):
     s, b = map(int, input().split())
     source.append([s, b])
 
-arr = [[] for _ in range(n+1)]
-arr[0].append((1, 0))
-'''
-for i in range(1, len(source)+1): # 1 ~ 2
-    for pre_source in source: # 3,8 / 5,8
-        for pre_s, pre_b in arr[i-1]:
-            arr[i].append((pre_s*pre_source[0], pre_b+pre_source[1]))
-'''
 prev = [(1, 0)]
 for s, b in source:
     new = prev[:]
@@ -25,10 +17,9 @@ for s, b in source:
         new.append((ps * s, pb + b))
     prev = new 
     
-arr = arr[n]
 answer = 1000000000
 del prev[0]
-#print(prev)
+
 for i in prev:
     if abs(i[0] - i[1]) < answer:
         answer = abs(i[0] - i[1])
