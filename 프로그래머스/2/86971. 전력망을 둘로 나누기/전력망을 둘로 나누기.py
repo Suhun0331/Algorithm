90addc4c4ad639702f24983c123e07a3917b010a
SuHun <104614675+Suhun0331@users.noreply.github.com>
2025-02-13 15:49:02 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.11 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,32 +1,30 @@
-import copy
-
 def dfs(num, arr, visited):
     visited[num] = True
-    count = 1
+    count = 1 
     for i in arr[num]:
         if not visited[i]:
             count += dfs(i, arr, visited)
-            
     return count
-    
-        
+
 def solution(n, wires):
     arr = [[] for _ in range(n+1)]
-    answer = 100
-    for i in wires:
-        arr[i[0]].append(i[1])
-        arr[i[1]].append(i[0])
-    for i in wires:
-        visited = [False] * (n+1)
-        copy_arr = copy.deepcopy(arr)
-        copy_arr[i[0]].remove(i[1])
-        copy_arr[i[1]].remove(i[0])
+    answer = float('inf')  
+    
+    for a, b in wires:
+        arr[a].append(b)
+        arr[b].append(a)
+    
+    for a, b in wires:
+        visited = [False] * (n + 1)
         
-        cnt1 = dfs(i[0], copy_arr, visited)
-        cnt2 = dfs(i[1], copy_arr, visited)
-        
+        arr[a].remove(b)
+        arr[b].remove(a)
+
+        cnt1 = dfs(a, arr, visited)
+        cnt2 = n - cnt1
         answer = min(answer, abs(cnt1 - cnt2))
+
+        arr[a].append(b)
+        arr[b].append(a)
         
     return answer
-        
-        
