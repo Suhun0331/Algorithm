53b3b0f9ea9fd0d83e6e28c236e7a607f6af6b53
SuHun <104614675+Suhun0331@users.noreply.github.com>
2025-02-14 11:41:55 +0900

    [level 2] Title: 모음 사전, Time: 3.22 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,23 +1,28 @@
 '''
-itertools 중에 product라는 애 사용하면 될듯
-
-이거 되면 dfs로도 풀어봐야지
+dfs 풀이
 '''
-from itertools import product
 
 def solution(word):
+    cnt = 0
     alpha = ['A', 'E', 'I', 'O', 'U']
-    arr = []
-    final = []
-    for i in range(1, 6):
-        arr.extend(list(product(alpha, repeat = i)))
-        
-    for i in arr:
-        final.append(''.join(map(str, i)))
-    final.sort()
-    #print(final[:10])
     
-    return final.index(word)+1
+    def dfs(arr):
+        nonlocal cnt
+        cnt += 1
+        if word == arr:
+            return True
+        if len(arr) == 5:
+            return False
+
+        for i in alpha:
+            if dfs(arr+i):
+                return True
+            list(arr).pop()
+        
+    for i in alpha:
+        if dfs(i):
+            return cnt
+    
         
         
         
