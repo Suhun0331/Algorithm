1dfce6e57f7eaa166a0cd4cf2de0da2bcc8224f4
SuHun <104614675+Suhun0331@users.noreply.github.com>
2025-02-19 11:10:51 +0900

    [level 3] Title: 단속카메라, Time: 2.65 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -1,7 +1,7 @@
 '''
 시작점 기준 정렬 or 끝 기준 정렬? or 절댓값 작은 기준 정렬?
 
-1. 시작점 기준 정렬
+1. 시작점 기준 정렬 -> x
 -20 -18 -14 -5
 
 첫 번째 타고 올라가는데, 첫 번째 범위 중 가장 뒷부분에 카메라를 설치하는게 좋음.
@@ -9,7 +9,7 @@
 a. 첫 번째 범위 중 가장 뒤 + 두 번째 범위의 끝 이전
 b. 이때 설치한 카메라의 위치가 그 뒷 범위에 속하면 continue, 아니면 다시 a부터 시작
 
-2. 끝지점 기준 정렬
+2. 끝지점 기준 정렬 -> o
 -15 -13 -5 -3
 '''
 
@@ -25,21 +25,12 @@ def solution(routes):
             visited[i] = True
             continue
             
-        if routes[i][1] < routes[i+1][0]:
-            answer += 1
-            visited[i] = True
-            point = routes[i][1]
-        elif routes[i][1] >= routes[i+1][1]:
-            visited[i] = True
-        elif routes[i][1] == routes[i+1][0]:
-            point = routes[i][1]
-            answer += 1
-            visited[i], visited[i+1] = True, True
-        else:
-            point = max(routes[i][1] , routes[i+1][0])
-            visited[i] = True
+        if routes[i][1] > routes[i+1][0]:
             visited[i+1] = True
-            answer += 1
+            
+        point = routes[i][1]
+        answer += 1
+        visited[i] = True
         
     if routes[-1][0] > point:
         answer += 1
