6c18bdf6062c2f3d40e800592671fcd0adce045b
SuHun <104614675+Suhun0331@users.noreply.github.com>
2025-03-05 14:53:12 +0900

    [level 3] Title: 등굣길, Time: 3.32 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.py
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -3,30 +3,18 @@ def solution(m, n, puddles):
     for i in puddles:
         x, y = i
         map[y][x] = -1
-    for i in range(1, m+1):
-        if map[1][i] != -1:
-            map[1][i] = 1
-        else:
-            break
-    for i in range(1, n+1):
-        if map[i][1] != -1:
-            map[i][1] = 1
-        else:
-            break
         
-    print(map)
-    for i in range(2, n+1):
-        for j in range(2, m+1):
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            if i == j == 1:
+                map[j][i] = 1
+                continue
             if map[i][j] == -1:
                 continue
-            if map[i-1][j] == -1 and map[i][j-1] == -1:
-                map[i][j] = 0
             elif map[i-1][j] == -1:
                 map[i][j] = map[i][j-1]
             elif map[i][j-1] == -1:
                 map[i][j] = map[i-1][j]
             else:
-                map[i][j] = map[i-1][j] + map[i][j-1]
-    print(map)
-            
-    return map[n][m]%1000000007
+                map[i][j] = (map[i-1][j] + map[i][j-1])%1000000007
+    return map[n][m]
