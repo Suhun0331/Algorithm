5185c1cc897cf7cedffdeac3751f1984f7232d53
SuHun <104614675+Suhun0331@users.noreply.github.com>
2025-03-05 13:42:11 +0900

    [level 3] Title: 정수 삼각형, Time: 65.48 ms, Memory: 13.1 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -5,15 +5,12 @@
 맨 앞과 맨 뒤는 값 하나밖에 못 옴.
 '''
 def solution(triangle):
-    lst = [[0]*(i+1) for i in range(len(triangle))]
-    lst[0][0] = triangle[0][0]
     for i in range(1, len(triangle)):
-        for j in range(len(triangle[i])):
+        for j in range(i+1):
             if j == 0:
-                lst[i][j] = lst[i-1][j] + triangle[i][j]
+                triangle[i][j] += triangle[i-1][j]
             elif j == len(triangle[i])-1:
-                lst[i][j] = lst[i-1][j-1] + triangle[i][j]
+                triangle[i][j] += triangle[i-1][j-1]
             else:
-                lst[i][j] = max(lst[i-1][j-1], lst[i-1][j]) + triangle[i][j]
-    answer = max(lst[-1])
-    return answer
+                triangle[i][j] += max(triangle[i-1][j-1], triangle[i-1][j])
+    return max(triangle[-1])
