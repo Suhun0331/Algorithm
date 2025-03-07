b7203f5b5ab6a215792814c44d9e3c500de9a53b
SuHun <104614675+Suhun0331@users.noreply.github.com>
2025-03-07 14:26:48 +0900

    [level 3] Title: 여행경로, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,39 +1,18 @@
-'''
-속도 확인용 복붙 풀이(내가 푼 거 x)
-'''
-
-from collections import defaultdict 
-
-def dfs(graph, N, key, footprint):
-    print(footprint)
-
-    if len(footprint) == N + 1:
-        return footprint
-
-    for idx, country in enumerate(graph[key]):
-        graph[key].pop(idx)
-
-        tmp = footprint[:]
-        tmp.append(country)
-
-        ret = dfs(graph, N, country, tmp)
-
-        graph[key].insert(idx, country)
-
-        if ret:
-            return ret
-
+from collections import defaultdict
 
 def solution(tickets):
-    answer = []
+    r = defaultdict(list)
+    for i,j in tickets:
+        r[i].append(j)
+    for i in r.keys():
+        r[i].sort()
 
-    graph = defaultdict(list)
-
-    N = len(tickets)
-    for ticket in tickets:
-        graph[ticket[0]].append(ticket[1])
-        graph[ticket[0]].sort()
-
-    answer = dfs(graph, N, "ICN", ["ICN"])
-
-    return answer
+    s = ["ICN"]
+    p = []
+    while s:
+        q = s[-1]
+        if r[q] != []:
+            s.append(r[q].pop(0))
+        else:
+            p.append(s.pop())
+    return p[::-1]
