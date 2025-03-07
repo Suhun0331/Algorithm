cf5699d86b59825fa0f1896f7de2a37abc10082b
SuHun <104614675+Suhun0331@users.noreply.github.com>
2025-03-07 13:35:35 +0900

    [level 3] Title: 여행경로, Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,34 +1,39 @@
 '''
-bfs dfs 둘 다 괜찮을 듯?
-일단 dfs로.
-방문한 지역 또 방문할 수도 있고
-dfs로 하나씩 들어갈 때마다 사용한 티켓 지웠다가, dfs 나오면 다시 티켓 추가하기
-len(티켓) == 0이면 모두 방문했다는 뜻이니까 해당 경로 answer에 append
-마지막에 answer 정렬한 후 맨 앞 리스트 return
-
-16번째줄 append 하는 조건을 수정해야 함. 티켓을 모두 사용했을 때를 구해야하니까
-
+속도 확인용 복붙 풀이(내가 푼 거 x)
 '''
-def dfs(current, route, tickets, visited, count, answer):
-    if count == len(tickets):
-        answer.append(route[:])
-        return
-    for i in range(len(tickets)):
-        if current == tickets[i][0] and not visited[i]:
-            route.append(tickets[i][1])
-            visited[i] = True
-            count += 1
-            dfs(tickets[i][1], route, tickets, visited, count, answer)
-            count -= 1
-            visited[i] = False
-            route.pop()
-            
+
+from collections import defaultdict 
+
+def dfs(graph, N, key, footprint):
+    print(footprint)
+
+    if len(footprint) == N + 1:
+        return footprint
+
+    for idx, country in enumerate(graph[key]):
+        graph[key].pop(idx)
+
+        tmp = footprint[:]
+        tmp.append(country)
+
+        ret = dfs(graph, N, country, tmp)
+
+        graph[key].insert(idx, country)
+
+        if ret:
+            return ret
+
 
 def solution(tickets):
-    visited = [False] * (len(tickets)+1)
-    route = ["ICN"]
-    count = 0
     answer = []
-    dfs("ICN", route, tickets, visited, count, answer)
-    answer.sort()
-    return answer[0]
+
+    graph = defaultdict(list)
+
+    N = len(tickets)
+    for ticket in tickets:
+        graph[ticket[0]].append(ticket[1])
+        graph[ticket[0]].sort()
+
+    answer = dfs(graph, N, "ICN", ["ICN"])
+
+    return answer
