9a992f461662b98be42a00a8774e5632dffb1107
SuHun <104614675+Suhun0331@users.noreply.github.com>
2025-03-11 16:25:25 +0900

    [level 3] Title: 아이템 줍기, Time: 20.66 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
@@ -1,55 +1,61 @@
+'''
+2차원 배열 만들어서 테두리만 표시? / 전체 다 표시 후 테두리만 따라가기?
+테두리만 표시하는게 효율적일 듯.
+사각형 입력 받으면 각 사각형 전체 다 1로 표시한 후에
+전체 반복문 돌면서 테두리만 남기기(1인 칸 중 상하좌우 모두 1이면 테두리 x)
+그 후 두 방향 중 더 가까운 방향 선택.
+'''
 import copy
 
 def solution(rectangle, characterX, characterY, itemX, itemY):
     global answer
-    answer = float('inf')  # 최솟값을 찾기 위해 무한대로 초기화
-    N = 52 * 2  # 2배 확장한 좌표 크기
-
-    # 1. 2D 배열을 만들어 사각형 내부를 1로 채우기
-    arr = [[0] * N for _ in range(N)]
+    answer = float('inf')
+    cnt = 0
+    arr = [[0]*(52*2) for _ in range(52*2)]
     
-    for x1, y1, x2, y2 in rectangle:
-        x1, y1, x2, y2 = x1 * 2, y1 * 2, x2 * 2, y2 * 2
-        for x in range(x1, x2 + 1):
-            for y in range(y1, y2 + 1):
-                arr[x][y] = 1
-
-    # 2. 테두리만 남기기 (8방향 탐색)
+    for i in rectangle:
+        x1, y1, x2, y2 = i[0]*2, i[1]*2, i[2]*2, i[3]*2
+        for x in range(x1, x2+1):
+            for y in range(y1, y2+1):
+                if arr[x][y] != 1:
+                    arr[x][y] = 1
     dx = [1, 0, -1, 0, 1, 1, -1, -1]
     dy = [0, 1, 0, -1, 1, -1, 1, -1]
 
     copy_arr = copy.deepcopy(arr)
-    for i in range(1, N):
-        for j in range(1, N):
+    for i in range(1, 52*2):
+        for j in range(1, 52*2):
             if copy_arr[i][j] == 1:
-                is_inner = True  # 기본적으로 내부라고 가정
-                for k in range(8):  # 8방향 검사
+                side = False
+                for k in range(8):
                     xx, yy = i + dx[k], j + dy[k]
-                    if 0 <= xx < N and 0 <= yy < N:
-                        if copy_arr[xx][yy] == 0:  # 주변에 0이 하나라도 있으면 테두리임
-                            is_inner = False
+                    if 0<= xx < 52*2 and 0 <= yy < 52*2:
+                        if copy_arr[xx][yy] == 0:
+                            side = True
                             break
-                if is_inner:
-                    arr[i][j] = 0  # 내부라면 0으로 바꿔서 제거
-
-    # 3. DFS로 최단 거리 탐색
-    visited = [[False] * N for _ in range(N)]
-
+                if not side:
+                    arr[i][j] = 0
+                
+    visited = [[False]*52*2 for _ in range(52*2)]
+    
+    count = 0
     def dfs(x, y, count):
         global answer
-        if arr[x][y] == 0 or visited[x][y]:  # 테두리가 아니거나 이미 방문한 경우
+        if arr[x][y] == 0 or visited[x][y]:
             return
-        if (x, y) == (itemX * 2, itemY * 2):  # 목표 지점 도착
+        if (x, y) == (itemX*2, itemY*2):
             answer = min(answer, count)
             return
-
+        
         visited[x][y] = True
-        for i in range(4):  # 상하좌우 탐색
+        
+        for i in range(4):
             xx, yy = x + dx[i], y + dy[i]
-            if 0 <= xx < N and 0 <= yy < N and not visited[xx][yy] and arr[xx][yy] == 1:
-                dfs(xx, yy, count + 1)
-        visited[x][y] = False  # 백트래킹
-
-    dfs(characterX * 2, characterY * 2, 0)
-
-    return answer // 2  # 2배 확장했으므로 결과값도 2로 나눠서 반환
+            if 0<= xx < 52*2 and 0 <= yy < 52*2:
+                dfs(xx, yy, count+1)
+            # visited[xx][yy] = False
+            
+            
+    dfs(characterX*2, characterY*2, count)
+    
+    return answer//2
