5a82ff759abb4afc74769509b13527fb6106e279
SuHun <104614675+Suhun0331@users.noreply.github.com>
2025-03-12 13:59:26 +0900

    [Gold V] Title: 꿀 따기, Time: 184 ms, Memory: 42660 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/21758. 꿀 따기/꿀 따기.py
+++ 백준/Gold/21758. 꿀 따기/꿀 따기.py
@@ -1,26 +1,35 @@
 '''
-이분탐색? dp?
-어차피 꿀통까지 움직이니까 꿀통의 위치는 상관 없을 거 같고
-벌의 위치가 중요할 듯?
-전체 합에서 벌 위치 빼면 될 거 같음(조합)
-
-꿀통은 맨 오른쪽 / 맨 왼쪽 / 중간
+누적합.
+벌집이 맨 왼쪽 / 맨 오른쪽 / 가운데
+맨 왼쪽 -> 벌 하나는 무조건 맨 오른쪽,
+36 + 1 ~ 마지막 한 번씩 돌면서 누적합[i-1] + 36-arr[i] 의 최댓값
+맨 오른쪽도 마찬가지
+가운데 : (누적합[i]-누적합[0]) + (누적합[-1] - 누적합[i])의 최댓값 
 '''
-from itertools import combinations
 
-n= int(input())
-lst = list(map(int, input().split()))
-arr = list(i for i in range(n))
-comb = list(combinations(arr, 2))
+n = int(input())
+arr = list(map(int, input().split()))
 answer = 0
 
-for i in comb:
-    left, right = i
-    # 벌집이 맨 오른쪽
-    answer = max(answer, sum(lst[left:]) + sum(lst[right:]) - lst[left] - lst[right] * 2)
+sum_arr = [0] * len(arr)
+sum_arr[0] = arr[0]
+for i in range(1, len(arr)):
+    sum_arr[i] = sum_arr[i-1] + arr[i]
+
+for i in range(1, len(arr)-1):
     # 벌집이 맨 왼쪽
-    answer = max(answer, sum(lst[:left+1]) + sum(lst[:right]) - lst[left] * 2)
-    # 벌집이 가운데
-    answer = max(answer, sum(lst[left:right+1]) + max(lst[left:right]) - lst[left] - lst[right])
+    answer = max(answer, sum_arr[i-1] + sum_arr[-2] - arr[i])
+    #print(i, 'one', answer) # 1 101 102 103 104
+        
+    #벌집이 맨 오른쪽
+    answer = max(answer, sum_arr[-1] - arr[0] + sum_arr[-1]-sum_arr[i]-arr[i])
+    #print(i, 'two', answer)
+    #벌집이 가운데
+    answer = max(answer, sum_arr[i]-sum_arr[0] + sum_arr[-2]-sum_arr[i-1])
+    #print(i, 'three', answer) # 2 7 11 (7 - 2 + 11 - 7)
+    
+    #print(answer)
 
 print(answer)
+# 1 100 1 1 100 1 -> 204
+
