8d5831feb14d234682db8ae82ccff4a1ab2e5419
SuHun <104614675+Suhun0331@users.noreply.github.com>
2025-03-13 11:40:20 +0900

    [Silver III] Title: N과 M (5), Time: 120 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.py
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -2,16 +2,19 @@ n, m = map(int, input().split())
 arr = list(map(int ,input().split()))
 arr.sort()
 lst = []
+visited = [False] * (n+1)
 def dfs(startIdx):
     if len(lst) == m:
         print(' '.join(map(str, lst)))
         return
     
     for i in range(n):
-        if arr[i] not in lst:
+        if not visited[i]:
             lst.append(arr[i])
+            visited[i] = True
             dfs(i)
             lst.pop()
+            visited[i] = False
             
 dfs(0)
 
