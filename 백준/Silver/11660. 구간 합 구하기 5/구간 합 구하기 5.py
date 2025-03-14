3de945b5e35284086dd75e9437078de7ae2717e5
SuHun <104614675+Suhun0331@users.noreply.github.com>
2025-03-14 11:13:38 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1032 ms, Memory: 106832 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -16,33 +16,16 @@ arr = []
 for i in range(n):
     arr.append(list(map(int, input().split())))
 
-sumarr = [[0]*n for _ in range(n)]
+sumarr = [[0]*(n+1) for _ in range(n+1)]
 for i in range(n):
     for j in range(n):
-        if i == 0 and j == 0:
-            sumarr[i][j] = arr[i][j]
-        elif i == 0:
-            sumarr[i][j] = sumarr[i][j-1]+arr[i][j]
-        elif j == 0:
-            sumarr[i][j] = sumarr[i-1][j] +arr[i][j]
-        else:
-            sumarr[i][j] = sumarr[i-1][j] + sumarr[i][j-1] - sumarr[i-1][j-1]+arr[i][j]
+        sumarr[i][j] = sumarr[i-1][j] + sumarr[i][j-1] - sumarr[i-1][j-1]+arr[i][j]
 
 for i in range(m):
     answer = 0
     x1, y1, x2, y2 = map(int, input().split())
     
-    if (x1, y1) == (x2, y2):
-        answer = arr[x1-1][y1-1]
-    elif x1 == 1 and y1 == 1:
-        answer = sumarr[x2-1][y2-1]
-    elif x1 == 1:
-        answer = sumarr[x2-1][y2-1] - sumarr[x2-1][y1-2]
-    elif y1 == 1:
-        answer= sumarr[x2-1][y2-1] - sumarr[x1-2][y2-1]
-        #print(sumarr[x2-1][y2-1], sumarr[x1-2][y2-1])
-    else:
-        answer = sumarr[x2-1][y2-1]-sumarr[x1-2][y2-1]-sumarr[x2-1][y1-2]+sumarr[x1-2][y1-2]
+    answer = sumarr[x2-1][y2-1]-sumarr[x1-2][y2-1]-sumarr[x2-1][y1-2]+sumarr[x1-2][y1-2]
     print(answer)
 
     
