718be1654e1e642fe5ea059fcb9ab2fa05e67597
SuHun <104614675+Suhun0331@users.noreply.github.com>
2025-03-18 17:13:48 +0900

    [Gold V] Title: 용액, Time: 112 ms, Memory: 44748 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.py
@@ -0,0 +1,27 @@
+'''
+답 보고 풀었음
+
+'''
+n = int(input())
+
+arr = list(map(int, input().split()))
+
+left, right = 0, n-1
+value = float('inf')
+answer = [left, right]
+while left < right:
+    hap = arr[left] + arr[right]
+    if value > abs(hap):
+        value = abs(hap)
+        answer[0], answer[1] = left, right
+
+    if hap > 0:
+        right -= 1
+    elif hap < 0:
+        left += 1
+    else:
+        break
+
+    
+print(arr[answer[0]], arr[answer[1]])
+   
