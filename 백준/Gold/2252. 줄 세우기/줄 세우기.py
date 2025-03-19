77f62b49687f16921100fc270985a7996505bb53
SuHun <104614675+Suhun0331@users.noreply.github.com>
2025-03-19 16:53:25 +0900

    [Gold III] Title: 줄 세우기, Time: 2648 ms, Memory: 40172 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -0,0 +1,34 @@
+'''
+첫 위상정렬 문제, 인터넷 코드 보면서 따라해본거. 다음에 다시 풀어보기
+'''
+from collections import deque
+
+v, e = map(int, input().split())
+
+indegree = [0] * (v+1)
+graph = [[] for _ in range(v+1)]
+
+for i in range(e):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    indegree[b] += 1
+
+def topology_sort():
+    q = deque()
+    answer = []
+    for i in range(1, v+1):
+        if indegree[i] == 0:
+            q.append(i)
+
+    while q:
+        now = q.popleft()
+        answer.append(now)
+        for i in graph[now]:
+            indegree[i] -= 1
+            if indegree[i] == 0:
+                q.append(i)
+
+    for i in answer:
+        print(i, end = ' ')
+
+topology_sort()
