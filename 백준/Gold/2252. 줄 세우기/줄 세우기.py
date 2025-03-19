5c60a6c86d6553343f19a3c1aca0e2593bb7f3c0
SuHun <104614675+Suhun0331@users.noreply.github.com>
2025-03-19 16:59:49 +0900

    [Gold III] Title: 줄 세우기, Time: 196 ms, Memory: 40172 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -1,28 +1,39 @@
+'''
+첫 위상정렬 문제, 인터넷 코드 보면서 따라해본거. 다음에 다시 풀어보기
+'''
 import sys
 from collections import deque
 
-n, m = map(int, sys.stdin.readline().split())
+input = sys.stdin.readline
 
-graph = [[] for _ in range(n+1)]
-inDegree = [0 for _ in range(n+1)]
-queue = deque()
+v, e = map(int, input().split())
+
+indegree = [0] * (v+1)
+graph = [[] for _ in range(v+1)]
+
+for i in range(e):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    indegree[b] += 1
+
+q = deque()
 answer = []
 
-for i in range(m):
-    a, b = map(int, sys.stdin.readline().rstrip().split())
-    graph[a].append(b)
-    inDegree[b] += 1
+for i in range(1, v+1):
+    if indegree[i] == 0:
+        q.append(i)
 
-for i in range(1, n+1):
-    if inDegree[i] == 0:
-        queue.append(i)
+while q:
+    now = q.popleft()
+    answer.append(now)
+    for i in graph[now]:
+        indegree[i] -= 1
+        if indegree[i] == 0:
+            q.append(i)
 
-while queue:
-    tmp = queue.popleft()
-    answer.append(tmp)
-    for i in graph[tmp]:
-        inDegree[i] -= 1
-        if inDegree[i] == 0:
-            queue.append(i)
+for i in answer:
+    print(i, end = ' ')
 
-print(*answer)
+
+                
+    
