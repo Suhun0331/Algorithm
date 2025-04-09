2f82c9ff8bed2704b49c9e753a2ca47743e38a9f
SuHun <104614675+Suhun0331@users.noreply.github.com>
2025-04-09 11:18:13 +0900

    [level 2] Title: 주식가격, Time: 39.88 ms, Memory: 18 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/README.md
+++ 프로그래머스/2/42584. 주식가격/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주식가격 - 42584 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584#) 
 
 ### 성능 요약
 
-메모리: 19 MB, 시간: 33.30 ms
+메모리: 18 MB, 시간: 39.88 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 09일 11:13:40
+2025년 04월 09일 11:18:10
 
 ### 문제 설명
 
--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -5,33 +5,24 @@
 price로 들어온 값 스택에 저장하는데, 맨 위에 있는 값이 들어온 값보다 크면 맨 위 pop
 스택에 값 저장할 때 가격, 인덱스 같이 저장하고
 pop 하게되면 answer[인덱스] += (새 인덱스-pop한값 인덱스)
+
++ 약간의 코드 최적화 
 '''
 def solution(prices):
     answer = [0] * len(prices)
     stack = []
     
-    for i, val in enumerate(prices): # 1 5 6 3
-        #print(stack)
-        if i == 0 or not stack:
-            stack.append((val, i))
-            continue
-            
-        if val >= stack[-1][0]:
+    for i, val in enumerate(prices):
+        if i == 0 or not stack or val >= stack[-1][0]:
             stack.append((val, i))
         else:
-            while val < stack[-1][0] :
+            while stack and val < stack[-1][0] :
                 _, ind = stack.pop()
                 answer[ind] += (i - ind)
-                if not stack:
-                    break
             stack.append((val, i))
             
     for i in stack:
         value, index = i
         answer[index] += len(prices) - index - 1
+        
     return answer
-    
-    
-    
-    
-    
