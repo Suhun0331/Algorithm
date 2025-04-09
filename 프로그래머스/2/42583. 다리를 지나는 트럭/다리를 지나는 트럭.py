7898b6a1eff1a354a15e454f24a0f76a0c0a86d0
SuHun <104614675+Suhun0331@users.noreply.github.com>
2025-04-09 14:45:09 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.69 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,24 +1,43 @@
 '''
-다른사람 풀이 - 내 풀이랑 속도 비교용
+트럭이 움직인다는 생각보다 그냥 순간이동 한다고 생각
+트럭 2대 올라갈 수 있음 -> 다리길이 2 -> 점 2개라고 생각하기
+1초에 점 1개씩 이동
+
+큐 사용
+weight 변수에는 현재 다리 위에 올라가있는 무게 저장
+stack의 길이가 다리 길이보다 작거나 같을 때만 트럭 추가할 수 있음.
+
+if 다리길이보다 다리 위 트럭 개수가 더 적은지 and 지금 추가하려는 트럭 무게 더해도 되는지:
+된다면 stack에 트럭 추가
+안된다면 큐의 맨 앞에 있는 트럭 통과시키기
+
++ 코드 최적화 
 '''
 from collections import deque
 
 def solution(bridge_length, weight, truck_weights):
-    bridge = deque(0 for _ in range(bridge_length))
-    total_weight = 0
-    step = 0
-    truck_weights.reverse()
-
-    while truck_weights:
-        total_weight -= bridge.popleft()
-        if total_weight + truck_weights[-1] > weight:
-            bridge.append(0)
-        else:
-            truck = truck_weights.pop()
-            bridge.append(truck)
-            total_weight += truck
-        step += 1
-
-    step += bridge_length
-
-    return step
+    time = 1
+    q = deque()
+    onweight = 0
+    
+    for wei in truck_weights:
+        while True:
+            if q and time > q[0][1] + bridge_length:
+                onweight -= q[0][0]
+                q.popleft()
+            else:
+                break
+        while q and (onweight+wei > weight or len(q) == bridge_length):
+            print(time, 'check')
+            time += (bridge_length-time+q[0][1])
+            onweight -= q[0][0]
+            q.popleft()
+        q.append((wei, time))
+        time += 1
+        onweight += wei
+            
+    if q:
+        time += (bridge_length-time+q[-1][1])
+                
+    return time
+            
