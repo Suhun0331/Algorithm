65a0c1d7de5f453b1236412075e41778a3d4f489
SuHun <104614675+Suhun0331@users.noreply.github.com>
2025-04-28 11:36:42 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 96 ms, Memory: 35292 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -4,6 +4,8 @@ dp 문제? bfs / dfs?
 bfs 사용해서 앞 / 뒤 / x2 해서 들어가려는 초가 기존에 있던
 초보다 짧으면 새로 넣음
 그게 아니면 q에 안넣고 끝
++
+시간 줄이기 -> *2의 경우 appendleft 해서 먼저 처리 
 '''
 import sys
 from collections import deque
@@ -16,22 +18,26 @@ if n >= k:
     print(n-k)
     exit()
 
-count = [100001] * 100001
+count = [-1] * 100001
 time = 0
 count[n] = time
 q = deque()
 q.append(n)
-current = n
-def update(current, time):
-    if  0 <= current <= 100000:
-        if count[current] > time:
-            count[current] = time
-            q.append(current)
+
 while q:
     current = q.popleft()
+    if current == k:
+        break
     time = count[current]
-    update(current-1, time+1)
-    update(current+1, time+1)
-    update(current*2, time)
+    if current * 2 <= 100000 and count[current*2] == -1:
+        count[current*2] = time
+        q.appendleft(current*2)
+    if current-1 >= 0 and count[current-1] == -1:
+        count[current-1] = time+1
+        q.append(current-1)
+    if current + 1 <= 100000 and count[current+1] == -1:
+        count[current+1] = time+1
+        q.append(current+1)
+    
         
 print(count[k])
