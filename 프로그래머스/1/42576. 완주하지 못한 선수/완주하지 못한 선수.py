a3307b5417228e6d9e3f47f583b05cafc992a693
SuHun <104614675+Suhun0331@users.noreply.github.com>
2025-05-22 21:32:37 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 85.61 ms, Memory: 25.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,9 +1,10 @@
 def solution(participant, completion):
+    answer = ''
     participant.sort()
     completion.sort()
     
-    for p, c in zip(participant, completion):
-        if p != c:
-            return p  # 차이가 나는 순간 반환
+    for i, j in zip(participant, completion):
+        if i!=j:
+            return i
     
-    return participant[-1]  # 마지막 남은 선수 반환
+    return participant[-1]
