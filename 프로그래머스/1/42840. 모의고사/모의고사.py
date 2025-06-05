d8ba0863e94e303ebfe44c8c573992c78d245653
SuHun <104614675+Suhun0331@users.noreply.github.com>
2025-06-05 12:42:46 +0900

    [level 1] Title: 모의고사, Time: 3.61 ms, Memory: 9.47 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,26 +1,23 @@
 def solution(answers):
-
+    answer = []
     one = [1, 2, 3, 4, 5]
     two = [2, 1, 2, 3, 2, 4, 2, 5]
     three = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    index = 0
-    score = [0, 0, 0]
-    for i in answers:
-        if i == one[index%5]:
-            score[0] += 1
-        if i == two[index%8]:
-            score[1] += 1
-        if i == three[index%10]:
-            score[2] += 1
-        index += 1
-    check = max(score)
-    answer = []
-    if score[0] == check:
+    onescore, twoscore, threescore = 0, 0, 0
+    for i in range(len(answers)): # 5
+        if answers[i] == one[i%len(one)]:
+            onescore += 1
+        if answers[i] == two[i%len(two)]:
+            twoscore += 1
+        if answers[i] == three[i%len(three)]:
+            threescore += 1
+    
+    maxnum = max(onescore, twoscore, threescore)
+    if maxnum == onescore:
         answer.append(1)
-    if score[1] == check:
+    if maxnum == twoscore:
         answer.append(2)
-    if score[2] == check:
+    if maxnum == threescore:
         answer.append(3)
     return answer
-    
-    
+        
