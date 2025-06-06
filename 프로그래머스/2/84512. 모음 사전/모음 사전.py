2935ad943b56ab51635ebc5b5ad2027918a447a2
SuHun <104614675+Suhun0331@users.noreply.github.com>
2025-06-06 19:09:02 +0900

    [level 2] Title: 모음 사전, Time: 1.26 ms, Memory: 9.15 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,29 +1,20 @@
-'''
-dfs 풀이
-'''
-
 def solution(word):
-    cnt = 0
     alpha = ['A', 'E', 'I', 'O', 'U']
-    
-    def dfs(arr):
-        nonlocal cnt
-        cnt += 1
-        if word == arr:
-            return True
-        if len(arr) == 5:
-            return False
-
-        for i in alpha:
-            if dfs(arr+i):
-                return True
-            list(arr).pop()
-        
-    for i in alpha:
-        if dfs(i):
-            return cnt
-    
-        
-        
-        
-        
+    count = 0
+    answer = 0
+    find = False
+    def dfs(sentence):
+        nonlocal answer, find, count
+        if find:
+            return
+        if sentence == word:
+            answer = count
+            find = True
+            return
+        count += 1
+        if len(sentence) == 5:
+            return
+        for i in range(5):
+            dfs(sentence+alpha[i])
+    dfs('')
+    return answer
