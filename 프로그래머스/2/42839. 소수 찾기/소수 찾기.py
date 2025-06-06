4665c124deba1fe5143909f194c845457487d195
SuHun <104614675+Suhun0331@users.noreply.github.com>
2025-06-06 14:40:16 +0900

    [level 2] Title: 소수 찾기, Time: 1022.13 ms, Memory: 9.44 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,34 +1,34 @@
 '''
-일단 일의자리에 짝수 오면 x
-소수 구하는 규칙? 2 ~ n-1로 나눴을 때 나눠떨어지면 안됨
-일단 만들 수 있는 숫자 다 구하고 배열 돌면서 하나씩 확인 해야하나
-숫자 중복 있으면 안되니까 set 사용
-'''
+소수 -> 2부터 본인//2 까지 나누어떨어지지 않으면 소수
 
+흩어진 숫자들로 하나의 숫자 만들기 -> dfs?
+순열 사용해서 모든 경우의 수 뽑고 그거 int로 만들기
+'''
 from itertools import permutations
 
 def solution(numbers):
-    answer = 0
-    arr = []
-    for i in numbers:
-        arr.append(int(i))
+    count = 0
+    numlst = set()
     
-    num = set()
-    for i in range(1, len(arr)+1):# 1, 2, 3 -> 1 ~ 3
-        for j in permutations(arr, i):
-            num.add(int(''.join(map(str, j))))
-    num = list(num)
-    print(num)
-
-    for i in num:
-        if i == 0 or i == 1:
+    for i in range(1, len(numbers)+1):
+        lst = list(permutations(list(numbers), i))
+        for j in lst:
+            number = int(''.join(j))
+            numlst.add(number)
+    # print(numlst)
+    for i in numlst:
+        sosu = True
+        if i == 1 or i == 0:
             continue
-        check = True
-        for j in range(2, i):
+        for j in range(2, i//2+1):
             if i%j == 0:
-                check = False
+                sosu = False
                 break
-        if check == True:
-            answer += 1
+        if sosu:
+            count += 1
+    return count
     
-    return answer
+    
+    
+    
+    
