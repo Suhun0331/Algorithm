fedb9b0ae6fa7978779321e0c9279943d2e43b9a
SuHun <104614675+Suhun0331@users.noreply.github.com>
2025-08-08 14:05:37 +0900

    [D5] Title: 수제 버거 장인, Time: 120 ms, Memory: 27,264 KB -BaekjoonHub

---
+++ SWEA/D5/3421. 수제 버거 장인/수제 버거 장인.java
@@ -0,0 +1,105 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.util.*;
+import java.io.*;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Solution
+{
+	static int count;
+	static int[] arr;
+	static boolean[] visited;
+	static int n;
+	static int m;
+	static boolean[][] map;
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st;
+		
+		int T = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		for (int t = 0; t < T; t++) {
+			
+			count = 1;
+			
+			st = new StringTokenizer(br.readLine());
+			n = Integer.parseInt(st.nextToken());
+			m = Integer.parseInt(st.nextToken());
+//			arr = new int[n];
+			map = new boolean[n+1][n+1];
+			visited = new boolean[n+1];
+			
+			for (int i = 0; i < m; i++) {
+				st = new StringTokenizer(br.readLine());
+				int num1 = Integer.parseInt(st.nextToken());
+				int num2 = Integer.parseInt(st.nextToken());
+				map[num1][num2] = true;
+				map[num2][num1] = true;
+			}
+			
+			dfs(1, 0); // (start, depth)
+			
+			sb.append("#").append(t+1).append(" ").append(count).append("\n");
+			
+		}
+		System.out.print(sb);
+		
+	}
+	
+	public static void dfs(int start, int depth) {
+		
+		for (int i = start; i <= n; i++) {
+			
+			
+			if(!visited[i]) {
+				
+				boolean conflict = false;
+		        for (int j = 1; j <= n; j++) {
+		            if (visited[j] && map[i][j]) { // i와 j가 같이 못 쓰는 쌍이면
+		                conflict = true;
+		                break;
+		            }
+		        }
+		        if (conflict) continue;
+				
+				
+				count += 1;
+				visited[i] = true;
+				dfs(i, depth+1);
+				visited[i] = false;
+			}
+		}
+		
+	}
+}
