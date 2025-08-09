2150f1b755d1aeef169767931c9bacf93abf6d2a
SuHun <104614675+Suhun0331@users.noreply.github.com>
2025-08-09 16:59:44 +0900

    [Silver II] Title: 결혼식, Time: 160 ms, Memory: 18416 KB -BaekjoonHub

--- 백준/Silver/5567. 결혼식/결혼식.java
+++ 백준/Silver/5567. 결혼식/결혼식.java
@@ -1,64 +1,57 @@
-/**
- * 입력 다 받고 1의 친구 array에 저장
- * array 돌면서 이 안에 들어있는 사람들의 친구 수 count + 방문처리
- * 
- */
-
 import java.util.*;
 import java.io.*;
 
 public class Main {
-	static boolean[] visited;
+	
+	static List<Integer>[] arr;
+	static boolean[] check;
 	static int count;
 	
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
-		
-		ArrayList<Integer> arr = new ArrayList<>();
 		
 		int n = Integer.parseInt(br.readLine());
 		int m = Integer.parseInt(br.readLine());
+		check = new boolean[n+1];
 		
-		boolean[][] friend = new boolean[n+1][n+1];
-		visited = new boolean[n+1];
-		count = 0;
+		arr = new ArrayList[n+1];
+		for (int i = 0; i < n+1; i++) {
+			arr[i] = new ArrayList<>();
+		}
 		
 		for (int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
-			int num1 = Integer.parseInt(st.nextToken());
-			int num2 = Integer.parseInt(st.nextToken());
-			friend[num1][num2] = true;
-			friend[num2][num1] = true; 
+			
+			int n1 = Integer.parseInt(st.nextToken());
+			int n2 = Integer.parseInt(st.nextToken());
+			arr[n1].add(n2);
+			arr[n2].add(n1);
 		}
 		
+		dfs(0, 1);//(depth, start)
 		
-		for (int i = 2; i < n+1; i++) {
-			if(friend[1][i]) {
-				//어레이에 i 추가 -> i는 1번의 친구
-				arr.add(i);
-				visited[i] = true;
-				count += 1;
-			}
+		for (int i = 2; i < check.length; i++) {
+			if(check[i]) count += 1;
 		}
-		
-		for(int i : arr) { // i = 2, 3
-			for (int j = 2; j < n+1; j++) {
-				if(friend[i][j] && !visited[j] && i != j) {
-					visited[j] = true;
-//					System.out.println(j);
-					count += 1;
-				}
-			}
-		}
-		
-//		for (int i = 0; i < visited.length; i++) {
-//			System.out.print(visited[i] + " ");
-//		}
-		
 		
 		System.out.println(count);
+		
 	}
+	
+	public static void dfs(int depth, int start) { // 01 , 1 2, 2 3 , 
+		if(depth == 2) return;
+		
+		for (int i : arr[start]) {
+			
+			check[i] = true;
+			dfs(depth+1, i);
+			
+		}
+		
+		
+	}
+	
 	
 }
