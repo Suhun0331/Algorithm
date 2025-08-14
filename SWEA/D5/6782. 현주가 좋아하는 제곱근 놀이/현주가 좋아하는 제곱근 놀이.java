a6a82e49f686b911e35110b9af6acb4c100f1dfe
SuHun <104614675+Suhun0331@users.noreply.github.com>
2025-08-14 11:54:43 +0900

    [D5] Title: 현주가 좋아하는 제곱근 놀이, Time: 492 ms, Memory: 33,280 KB -BaekjoonHub

---
+++ SWEA/D5/6782. 현주가 좋아하는 제곱근 놀이/현주가 좋아하는 제곱근 놀이.java
@@ -0,0 +1,76 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.io.*;
+import java.util.*;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Solution
+{
+	public static void main(String[] Args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int t = 0; t < T; t++) {
+			Long n = Long.parseLong(br.readLine());
+			int answer = 0;
+			
+			while(n != 2) {
+				if(n == 1) {
+					answer += 1;
+				}
+				else {
+					long r = (long) Math.sqrt(n);
+					if(r*r == n) {
+						answer += 1;
+						n = r;
+					}
+					else {
+						long next = (r+1) * (r+1);
+						
+						Long diff = next - n;
+						answer += diff;
+						n = next;
+						
+					}
+				}
+			}
+			
+			sb.append("#").append(t+1).append(" ").append(answer).append("\n");
+		}
+		
+		System.out.print(sb);
+
+	}
+}
