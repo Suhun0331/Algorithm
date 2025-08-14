cc07e2646b97299a848d83e30804a04d4d5c2147
SuHun <104614675+Suhun0331@users.noreply.github.com>
2025-08-14 14:26:21 +0900

    [D4] Title: 정사각형 방, Time: 124 ms, Memory: 34,816 KB -BaekjoonHub

--- SWEA/D4/1861. 정사각형 방/정사각형 방.java
+++ SWEA/D4/1861. 정사각형 방/정사각형 방.java
@@ -26,68 +26,79 @@
 //System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
 /////////////////////////////////////////////////////////////////////////////////////////////
 import java.io.*;
+import java.util.*;
 
-public class Solution {
-    static int N;
-    static int [][] arr;
-    static int maxDepth;
-    static boolean [][] visited;
-    static int [] dx = {1, 0, -1, 0};
-    static int [] dy = {0, 1, 0, -1};
+class Solution {
+	static int arr[][];
+	static int dp[][];
+	static int dr[] = { 0, 0, -1, 1 };
+	static int dc[] = { -1, 1, 0, 0 };
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
+	static int N;
 
-        for (int tc = 1; tc <= T; tc++) {
-            // 입력
-            N = Integer.parseInt(br.readLine());
-            arr = new int [N][N];
-            for (int i = 0; i < N; i++) {
-                String [] s = br.readLine().split(" ");
-                for (int j = 0; j < N; j++) {
-                    arr[i][j] = Integer.parseInt(s[j]);
-                }
-            }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
 
-            // 초기화
-            int answer = 0;
-            int startPoint = 0;
-            visited = new boolean [N][N];
+		for (int test_case = 1; test_case <= T; test_case++) {
+			N = Integer.parseInt(br.readLine());
+			arr = new int[N][N];
+			dp = new int[N][N];
 
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    if (!visited[i][j]) {
-                        maxDepth = 0;
-                        dfs(i, j, 1); // i, j가 추가되었으므로 depth는 1부터 시작
-                        if (maxDepth == answer)
-                            startPoint = Math.min(startPoint, arr[i][j]); // 여러개 가능하면 값이 더 작은 쪽으로
-                        else if (maxDepth > answer) {
-                            answer = maxDepth; // answer 갱신
-                            startPoint = arr[i][j];
-                        }
-                    }
-                }
-            }
-            System.out.println("#" + tc + " " + startPoint + " " + answer);
-        }
-    }
-    
-    public static void dfs(int x, int y, int depth) {
-        maxDepth = Math.max(depth, maxDepth);
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine(), " ");
+				for (int j = 0; j < N; j++) {
+					arr[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
 
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
+			int startNum = Integer.MAX_VALUE;
+			int max = Integer.MIN_VALUE;
 
-            if (!(0 <= nx && nx < N && 0 <= ny && ny < N))
-                continue;
-            
-            if (visited[nx][ny])
-                continue;
-            
-            if (arr[nx][ny] == arr[x][y] + 1)
-                dfs(nx, ny, depth + 1);
-        }
-    }
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					if (dp[i][j] == 0)
+						dfs(i, j);
+				}
+			}
+
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					if (max < dp[i][j]) {
+						startNum = arr[i][j];
+						max = dp[i][j];
+					} else if (max == dp[i][j]&& startNum > arr[i][j]) {
+						startNum = arr[i][j];
+						max = dp[i][j];
+					}
+				}
+			}
+
+			sb.append("#" + test_case + " " + startNum + " " + max + "\n");
+		}
+		System.out.println(sb);
+	}
+
+	public static int dfs(int row, int col) {
+		if (dp[row][col] != 0)
+			return dp[row][col];
+
+		dp[row][col] = 1;
+
+		for (int i = 0; i < 4; i++) {
+			int nr = row + dr[i];
+			int nc = col + dc[i];
+
+			if (nr < 0 || nr >= N || nc < 0 || nc >= N)
+				continue;
+
+			if (arr[row][col] + 1 == arr[nr][nc]) {
+				dp[row][col] = Math.max(dp[row][col], dfs(nr, nc) + 1);
+			}
+		}
+		return dp[row][col];
+	}
+
 }
